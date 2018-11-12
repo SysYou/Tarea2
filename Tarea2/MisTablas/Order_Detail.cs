e0fcd7758a59aa5c6af9fcf7bec741c2bd781f2f
Diego <diegoradriz@gmail.com>
2018-11-11 19:25:14 -0600

    .

--- Tarea2/MisTablas/Order_Detail.cs
+++ Tarea2/MisTablas/Order_Detail.cs
@@ -5,10 +5,24 @@ using System.Web;
 
 namespace Tarea2.MisTablas
 {
-    public partial class OrdersDetail
+    public partial class Order_Detail
     {
 
+        public int? Monto_Descuento
+        {
+            get
+            {
 
+               // int? Total = null;
+               // int? MontoDescuento = null;
+
+               // //[UnitPrice] * [Quantity]  as Total
+
+
+               //// Quantity + 1;
+
+            }
+        }
 
     }
     
